81e11413792e64a2d80bb38281718f07c66102f9
Claas <claas@claas.dev>
2024-05-26 19:13:58 +0200

    Complete signin flow
    Help users understand errors with their sign in links
    Send users to their personal surveys overview afters successful sign in
    Tell users to check their emails after signing in

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -20,6 +20,7 @@ use serde::Deserialize;
 pub(crate) mod authenticated_user;
 mod passkey;
 
+//TODO decide how long a session should live
 const SESSION_LIFETIME: time::Duration = time::Duration::days(30);
 const SIGNIN_ATTEMPT_LIFETIME: time::Duration = time::Duration::minutes(15);
 
@@ -94,15 +95,13 @@ async fn complete_signin(
         .unwrap();
 
     let Some(row) = rows.next().await.unwrap() else {
-        //TODO display error link has expired (even if it did not exist in the first place)
         // Don't give away if the link existed or not
-        return Redirect::to("/signin").into_response();
+        return Redirect::to("/signin/expired").into_response();
     };
 
     let expires_at: i64 = row.get(1).unwrap();
     if expires_at < time::OffsetDateTime::now_utc().unix_timestamp() {
-        //TODO display error link has expired
-        return Redirect::to("/signin").into_response();
+        return Redirect::to("/signin/expired").into_response();
     }
 
     let researcher_id: String = row.get(0).unwrap();
@@ -118,7 +117,6 @@ async fn complete_signin(
 
     // Create session
     let session_id = nanoid!();
-    //TODO decide how long a session should live
     let expires_at = time::OffsetDateTime::now_utc() + SESSION_LIFETIME;
 
     connection
@@ -138,11 +136,14 @@ async fn complete_signin(
     let cookie = Cookie::build(("session", session_id))
         .path("/")
         .secure(true)
+        // Tell browsers to not allow JavaScript to access the cookie. Prevents some XSS attacks
+        // (JS can still indirectly find out if user is authenticated by trying to access authenticated endpoints)
         .http_only(true)
+        // Prevents CRSF attack
         .same_site(SameSite::Strict)
         .expires(expires_at);
 
-    (jar.add(cookie), Redirect::to("/")).into_response()
+    (jar.add(cookie), Redirect::to("/surveys")).into_response()
 }
 
 #[derive(Template)]
@@ -154,25 +155,74 @@ async fn signup_completed() -> impl IntoResponse {
     sign_up_completed_template
 }
 
+#[derive(Template)]
+#[template(path = "signin_completed.html")]
+struct SigninCompletedTemplate {}
+
+async fn signin_completed() -> impl IntoResponse {
+    let sign_up_completed_template = SigninCompletedTemplate {};
+    sign_up_completed_template
+}
+
 #[derive(Deserialize, Debug)]
 struct SignInRequest {
-    username: String,
+    email: String,
 }
 
 async fn sign_in(
-    jar: CookieJar,
-    Form(sign_in_request): Form<SignInRequest>,
-) -> (CookieJar, Redirect) {
-    //TODO authentication
-    let cookie = Cookie::build(("user", sign_in_request.username))
-        .path("/")
-        .secure(true)
-        .http_only(true)
-        // Prevents CRSF attack
-        .same_site(SameSite::Strict)
-        .expires(time::OffsetDateTime::now_utc() + time::Duration::days(1));
+    State(AppState {
+        connection,
+        configuration,
+        client,
+    }): State<AppState>,
+    Form(request): Form<SignInRequest>,
+) -> Redirect {
+    // Get user id
+    let mut rows = connection
+        .query(
+            "SELECT id, email_address FROM researchers WHERE email_address = :email",
+            named_params![":email": request.email],
+        )
+        .await
+        .unwrap();
 
-    (jar.add(cookie), Redirect::to("/"))
+    let Some(row) = rows.next().await.unwrap() else {
+        // Navigate user to complete sign in even if they don't have an account
+        // This is to prevent snooping on which emails are registered
+        return Redirect::to("/signin/completed");
+    };
+
+    let user_id: String = row.get(0).unwrap();
+    let email_address: String = row.get(1).unwrap();
+
+    // Create signin attempt
+    let attempt_id = nanoid!();
+    let expires_at = time::OffsetDateTime::now_utc() + SIGNIN_ATTEMPT_LIFETIME;
+    let expires_at = expires_at.unix_timestamp();
+    dbg!(&attempt_id);
+    connection
+        .execute(
+            "INSERT INTO signin_attempts VALUES (:id, :researcher_id, :expires_at)",
+            named_params![
+                ":id": attempt_id.clone(),
+                ":researcher_id": user_id,
+                ":expires_at": expires_at,
+            ],
+        )
+        .await
+        .unwrap();
+
+    // Email address should be validated at this point
+    send_sign_in_email(
+        Email(email_address),
+        &client,
+        attempt_id,
+        configuration.server_url,
+    )
+    .await
+    .unwrap();
+
+    Redirect::to("/signin/completed")
 }
 
 #[derive(Template)]
@@ -194,11 +244,22 @@ async fn sign_in_handler() -> impl IntoResponse {
     sign_in_template
 }
 
+#[derive(Template)]
+#[template(path = "signin_expired.html")]
+struct SigninExpiredTemplate {}
+async fn sign_in_expired() -> impl IntoResponse {
+    let sign_in_expired_template = SigninExpiredTemplate {};
+
+    sign_in_expired_template
+}
+
 pub(crate) fn create_router() -> Router<AppState> {
     Router::new()
         .route("/signup", get(sign_up_handler).post(create_account))
         .route("/signup/completed", get(signup_completed))
         .route("/signin", get(sign_in_handler).post(sign_in))
+        .route("/signin/completed", get(signin_completed))
         .route("/signin/:attempt_id", get(complete_signin))
+        .route("/signin/expired", get(sign_in_expired))
         .route("/challenge", get(passkey::get_challenge))
 }
