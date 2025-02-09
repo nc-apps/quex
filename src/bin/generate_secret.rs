3f2b2fbde633ac3f844e63d78c603b489c04775a
Claas <claas@claas.dev>
2025-02-09 12:47:18 +0100

    Make size variable

--- src/bin/generate_secret.rs
+++ src/bin/generate_secret.rs
@@ -1,7 +1,10 @@
 use base64::{prelude::BASE64_URL_SAFE_NO_PAD, Engine};
 
 fn main() {
-    let mut key = [0u8; 32];
+    let mut arguments = std::env::args();
+    _ = arguments.next();
+    let argument = arguments.next().unwrap();
+    let mut key = vec![0u8; argument.parse().unwrap()];
     getrandom::getrandom(&mut key).unwrap();
     let encoded = BASE64_URL_SAFE_NO_PAD.encode(&key);
     println!("{}", encoded);
